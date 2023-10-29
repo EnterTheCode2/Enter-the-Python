4bd10039f8ba0f56f4e0fa058acb45812a469760
EnterTheCode2 <147319993+EnterTheCode2@users.noreply.github.com>
2023-10-29 19:05:43 +0900

    Delete Q01.py

--- Q01.py
+++
@@ -1,7 +0,0 @@
-a = "Life is too short, you need python"
-if "pants" in a: print("pants")
-elif "python" in a and "you" not in a: print("python")
-elif "shirt" not in a: print("shirt")
-elif "need" in a: print("need")
-else: print("none")
-# a 문자열 안에 pants 가 없으므로 shirt 출력
